d906047e30c605e66a77ea416f43acf20a1c43a3
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-07-27 17:03:24 -0700

    filters styles and layout

--- next-pizza/app/page.tsx
+++ next-pizza/app/page.tsx
@@ -1,4 +1,4 @@
-import { Title, Container, TopBar } from '@/components/shared';
+import { Title, Container, TopBar, Filters } from '@/components/shared';
 
 export default function Home() {
     return (
@@ -6,7 +6,20 @@ export default function Home() {
             <Container className="mt-10">
                 <Title text="All Pizzas" size="lg" className="font-extrabold" />
             </Container>
+
             <TopBar />
+
+            <Container className="mt-10 pb-14">
+                <div className="flex gap-[60px]">
+                    <div className="w-[250px]">
+                        <Filters />
+                    </div>
+
+                    <div className="flex-1">
+                        <div className="flex flex-col gap-16">Pizzas list</div>
+                    </div>
+                </div>
+            </Container>
         </>
     );
 }
---
+++ next-pizza/components/shared/filters.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Title } from './title';
+import { FilterCheckbox } from './filter-checkbox';
+import { Input } from '../ui';
+import { RangeSlider } from './range-slider';
+import { CheckboxFiltersGroup } from './checkbox-filters-group';
+
+interface Props {
+    className?: string;
+}
+
+export const Filters: React.FC<Props> = ({ className }) => {
+    return (
+        <div className={className}>
+            <Title text="Filters" size="sm" className="mb-5 font-bold" />
+
+            <div className="flex flex-col gap-4">
+                <FilterCheckbox text="Can add" value="1" />
+                <FilterCheckbox text="New additions" value="2" />
+            </div>
+
+            <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+                <p className="font-bold mb-3">Price min max:</p>
+                <div className="flex gap-3 mb-5">
+                    <Input type="number" placeholder="0" min={0} max={1000} defaultValue={0} />
+                    <Input type="number" placeholder="1000" min={100} max={1000} />
+                </div>
+
+                <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
+            </div>
+
+            <CheckboxFiltersGroup
+                title="Ingredients"
+                className="mt-5"
+                limit={6}
+                defaultItems={[
+                    { text: 'Mushrooms', value: '1' },
+                    { text: 'Cheese', value: '2' },
+                    { text: 'Tomato', value: '3' },
+                    { text: 'Bacon', value: '4' },
+                    { text: 'Olives', value: '5' },
+                    { text: 'Pepperoni', value: '6' },
+                ]}
+                items={[
+                    { text: 'Mushrooms', value: '1' },
+                    { text: 'Cheese', value: '2' },
+                    { text: 'Tomato', value: '3' },
+                    { text: 'Bacon', value: '4' },
+                    { text: 'Olives', value: '5' },
+                    { text: 'Pepperoni', value: '6' },
+                ]}
+            />
+        </div>
+    );
+};
