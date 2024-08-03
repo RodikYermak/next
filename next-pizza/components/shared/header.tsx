ee9182d17de06fc800a7ae4c097e03e09419b2db
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-08-02 21:59:49 -0700

    front end search for products

--- next-pizza/components/shared/header.tsx
+++ next-pizza/components/shared/header.tsx
@@ -1,26 +1,71 @@
-// import { cn } from '@lib/utils';
-import { cn } from '../../lib/utils';
+'use client';
+
+import { cn } from '@/lib/utils';
 import React from 'react';
 import { Container } from './container';
 import Image from 'next/image';
+import Link from 'next/link';
+import { SearchInput } from './search-input';
+// import { CartButton } from './cart-button';
+import { useRouter, useSearchParams } from 'next/navigation';
+// import toast from 'react-hot-toast';
+// import { ProfileButton } from './profile-button';
+// import { AuthModal } from './modals';
 import { Button } from '../ui';
 import { ArrowRight, ShoppingCart, User } from 'lucide-react';
 
 interface Props {
+    hasSearch?: boolean;
+    hasCart?: boolean;
     className?: string;
 }
 
-export const Header: React.FC<Props> = ({ className }) => {
+export const Header: React.FC<Props> = ({ hasSearch = true, hasCart = true, className }) => {
+    const router = useRouter();
+    const [openAuthModal, setOpenAuthModal] = React.useState(false);
+
+    const searchParams = useSearchParams();
+
+    React.useEffect(() => {
+        let toastMessage = '';
+
+        if (searchParams.has('paid')) {
+            toastMessage = 'Заказ успешно оплачен! Информация отправлена на почту.';
+        }
+
+        if (searchParams.has('verified')) {
+            toastMessage = 'Почта успешно подтверждена!';
+        }
+
+        // if (toastMessage) {
+        //     setTimeout(() => {
+        //         router.replace('/');
+        //         toast.success(toastMessage, {
+        //             duration: 3000,
+        //         });
+        //     }, 1000);
+        // }
+    }, []);
+
     return (
-        <header className={cn('border border-b', className)}>
+        <header className={cn('border-b', className)}>
             <Container className="flex items-center justify-between py-8">
-                <div className="flex items-center gap-4">
-                    <Image src="/logo.png" alt="Logo" width={35} height={35} />
-                    <div>
-                        <h1 className="text-2xl font-black">Next Pizza</h1>
-                        <p className="text-sm text-gray-400 leading-3">The best pizza in town</p>
+                {/* Левая часть */}
+                <Link href="/">
+                    <div className="flex items-center gap-4">
+                        <Image src="/logo.png" alt="Logo" width={35} height={35} />
+                        <div>
+                            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+                            <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+                        </div>
                     </div>
+                </Link>
+
+                <div className="mx-10 flex-1">
+                    <SearchInput />
                 </div>
+
+                {/* Правая часть */}
                 <div className="flex items-center gap-3">
                     <Button variant="outline" className="flex items-center gap-1">
                         <User size={16} />
